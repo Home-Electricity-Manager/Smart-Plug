d5999c59f7a52a7870c8e2d08ad72751185dd983
j-archit <57045366+j-archit@users.noreply.github.com>
2019-12-19 23:08:26 +0530

    1 current Bug
    Cannot Delay server response for incorrect wifi credentials.

--- Smart_Plug_Master.ino
+++ Smart_Plug_Master.ino
@@ -3,10 +3,9 @@
 */
 
 //Include Required Libraries
-#include<string.h>
-#include "servertext.h"
-#include<ESP8266WebServer.h>
-#include<ESP8266WiFi.h>
+#include "servertext.h"             // HTML for the Web Server
+#include <ESP8266WebServer.h>       
+#include <ESP8266WiFi.h>
 //#include<ESP8266WiFiMulti.h>
 //
 
@@ -14,9 +13,10 @@
 const char *softSSID = "Master";    //Credentials for Master Access Point
 const char *softPASS = "password";  //For the function to work, the password should be more than 8 chars and should begin with a char too
 
-char *ssid = "alok jain";
-char *pass = "9910138138";
-
+IPAddress softAP_ip(192,168,0,1);         //IP config for the soft Access Point
+IPAddress softAP_gateway(192,168,0,1);
+IPAddress softAP_subnet(255,255,255,0);
+ 
 int sflag = 0;
 //
 
@@ -29,7 +29,7 @@ ESP8266WebServer server(80);
 void sta_setup(char*, char*);
 
 void handle_root();
-void handle_notroot();
+void handle_notfound();
 void handle_disconnect_wifi();
 void handle_connect_wifi();
 void handle_wifi_login();
@@ -37,9 +37,10 @@ void handle_wifi_login();
 
 //Setup
 void setup() {
+
   //Pin Setup
-  pinMode(D0, OUTPUT);
-  digitalWrite(D0, HIGH);
+  pinMode(D4, OUTPUT);      //LED Indication for WiFi Connection
+  digitalWrite(D4, HIGH);
   
   // Variables
   int flag = 0;
@@ -49,69 +50,40 @@ void setup() {
   Serial.println("\nBegin Serial Communication With NodeMCU");
 
   //Begin WiFi Connections
-  //Begin WiFi Access Point Mode
   WiFi.mode(WIFI_AP_STA);
-  bool ret = WiFi.softAP(softSSID, softPASS);
+  //Begin WiFi Access Point Mode
+  WiFi.softAPConfig(softAP_ip, softAP_gateway, softAP_subnet);
+  bool ret = WiFi.softAP(softSSID, softPASS, 1, false, 5);
   if (ret == true)
   {
     Serial.print("\nSoft Access Point Started with IP: ");
     Serial.print(WiFi.softAPIP());
   }
-  
+  //Attempts WiFi Connection to the last connected network
+  WiFi.begin(); 
+
+  //HTML Server and Handles
   server.begin();
   Serial.print("\nHTTPS Server Started");
   server.on("/", HTTP_GET, handle_root);
   server.on("/disconnect_wifi", HTTP_POST, handle_disconnect_wifi);
   server.on("/connect_wifi", HTTP_POST, handle_connect_wifi);
   server.on("/wifi_login", handle_wifi_login);
-  server.onNotFound(handle_notroot);
-
-  WiFi.begin(); 
+  server.onNotFound(handle_notfound);
 }
 //
 
 //Main Loop
 void loop() {
-  if(WiFi.status() == WL_CONNECTED)
-    digitalWrite(D0, LOW);
+  if(WiFi.status() == WL_CONNECTED) //WiFi connection LED Indication
+    digitalWrite(D4, LOW);
   else
-    digitalWrite(D0, HIGH);
-  server.handleClient();        // Handle Incoming HTTP requests from Clients
+    digitalWrite(D4, HIGH);
+  server.handleClient();            // Handle Incoming HTTP requests from Clients
 }
 //
 
 // Function Definitions
-void sta_setup(char *s = ssid, char* p = pass)    
-{
-  /* 
-  *  For Setting up STA mode WiFi connections via user input.
-  *  Uses the HTML Server through the Soft Access Point to get WiFi network creds from 
-  *  user and setup the STA Mode Connection
-  */
-  WiFi.begin(s, p);
-  Serial.println("\nConnecting via STA mode");
-  while(WiFi.status() != WL_CONNECTED)
-  {
-    digitalWrite(D0, LOW);
-    delay(1000);
-    digitalWrite(D0, HIGH);
-    Serial.print(".");
-    sflag++;
-    if(sflag > 50)
-      break;
-  }
-  if(sflag < 50)
-  {
-    digitalWrite(D0, LOW);
-    Serial.print("Connection Established! IP: ");
-    Serial.print(WiFi.localIP());
-  }
-  else
-    Serial.print("Connection couldn't be Established ");
-  sflag = 0;
-  //WiFimulti_Object.addAP();
-}
-
 void handle_root()
 {
   if( WiFi.status() == WL_CONNECTED )
@@ -123,7 +95,8 @@ void handle_root()
 void handle_disconnect_wifi()
 {
   WiFi.disconnect();
-  digitalWrite(D0,HIGH);
+  digitalWrite(D4,HIGH);
+  Serial.println("WiFi Disconnected\n");
   server.sendHeader("Location", "/");
   server.send(303);
 }
@@ -139,24 +112,57 @@ void handle_connect_wifi()
 
 void handle_wifi_login()
 {
-  if(server.hasArg("SSID") && server.hasArg("pass"))
+  if(server.hasArg("ssid") && server.hasArg("pass"))
   {
-    std::string ssid_user("");
-    std::string pass_user("");
-//    strcpy( ssid_user, server.arg("SSID"));
-//    ssid_pass += server.arg("pass");
-    sta_setup();
-    if(WiFi.status() == WL_CONNECTED)
+    const String ssid_user(server.arg("ssid"));   // Recieve SSID and password from the User using the web application
+    const String pass_user(server.arg("pass"));
+
+    int i = 0;
+    while(ssid_user[i] != '\0')       //Parsing the SSID and storing it to a char pointer
+      i++;
+    char* s = new char[i+1];
+    for(int j=0; j<i; j++)
+    {
+      if(ssid_user[j]=='+')
+        s[j] = ' ';
+      else
+        s[j] = ssid_user[j];
+      s[j+1] = '\0';
+    }
+    Serial.print("\nSSID: ");
+    Serial.print(s);
+
+    i = 0;
+    while(pass_user[i] != '\0')       //Parsing the password and storing it to a char pointer
+      i++;
+    char* p = new char[i+1];
+    for(int j=0; j<i; j++)
+    {
+      if(pass_user[j]=='+')
+        p[j] = ' ';
+      else
+        p[j] = pass_user[j];
+      p[j+1] = '\0';
+    }
+    Serial.print("\nPass: ");
+    Serial.print(p);
+    
+    sta_setup(s, p);                  //Setup WiFi connection using the given credentials
+    
+    if(WiFi.status() == WL_CONNECTED) //Redirects to home page if Connectd successfully
     {
       server.sendHeader("Location", "/");
       server.send(303);
     }
-    else
-    {
+    else                              //Redirect to connect page if cannot connect. 
+    {                                 //BUG 1
       sflag = 1;
       server.sendHeader("Location", "/connect_wifi");
       server.send(303);
     }
+    
+    delete[] p;                       // Delete dynamically allocated memory
+    delete[] s;
   }
   else
   {
@@ -166,9 +172,38 @@ void handle_wifi_login()
   }
 }
 
-void handle_notroot()
+void handle_notfound()
 {
   server.sendHeader("Location", "/");
   server.send(303);
 }
+
+void sta_setup(char *s, char* p)    
+{
+  /* 
+  *  For Setting up STA mode WiFi connections via user input.
+  *  Uses the HTML Server through the Soft Access Point to get WiFi network creds from 
+  *  user and setup the STA Mode Connection
+  */
+  WiFi.begin(s, p);
+  Serial.println("\nConnecting via STA mode");
+  while(WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+    sflag++;
+    if(sflag > 40)
+      break;
+  }
+  if(sflag < 40)
+  {
+    digitalWrite(D4, LOW);
+    Serial.print("\nConnection Established! IP: ");
+    Serial.print(WiFi.localIP());
+  }
+  else
+    Serial.print("\nConnection couldn't be Established");
+  sflag = 0;
+}
+
 //
